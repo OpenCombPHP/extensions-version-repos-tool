2810253a60030d78d107f6266fd428adb02e909d
alee <zq@alee2002.com>
2012-03-20 10:18:21 +0800

    for framework upgrade 0.7

--- class/ReposAction.php
+++ class/ReposAction.php
@@ -2,7 +2,7 @@
 namespace org\opencomb\development\repos ;
 
 use org\jecat\framework\fs\imp\LocalFolder;
-use org\jecat\framework\fs\FileSystem;
+use org\jecat\framework\fs\Folder;
 use org\jecat\framework\message\Message;
 use org\opencomb\coresystem\mvc\controller\ControlPanel;
 
@@ -16,7 +16,7 @@ class ReposAction extends ControlPanel
 			return ;
 		}
 		
-		if( !$aReposFolder = FileSystem::singleton()->findFolder($this->params['path']) )
+		if( !$aReposFolder = Folder::singleton()->findFolder($this->params['path']) )
 		{
 			$this->createMessage(Message::error,"path : %s 不是一个存在的目录") ;
 			return ;
@@ -37,7 +37,7 @@ class ReposAction extends ControlPanel
 	
 	static public function chdir(LocalFolder $aFolder)
 	{
-		$sPath = preg_replace('|^file://|','',$aFolder->url()) ;
+		$sPath = preg_replace('|^file://|','',$aFolder->path()) ;
 		chdir($sPath) ;
 	}
 	
--- class/ReposStatus.php
+++ class/ReposStatus.php
@@ -4,7 +4,7 @@ namespace org\opencomb\development\repos ;
 use org\opencomb\coresystem\auth\Id;
 
 use org\jecat\framework\fs\imp\LocalFolder;
-use org\jecat\framework\fs\FileSystem;
+use org\jecat\framework\fs\Folder;
 use org\opencomb\platform\ext\ExtensionMetainfo;
 use org\opencomb\platform\ext\ExtensionManager;
 use org\opencomb\coresystem\mvc\controller\ControlPanel;
@@ -34,14 +34,14 @@ class ReposStatus extends ControlPanel
 		
 		$arrAllReposStatus['framework'] = array(
 				'type' => '框架' ,
-				'folder' => FileSystem::singleton()->findFolder('/framework') ,
+				'folder' => Folder::singleton()->findFolder('/framework') ,
 				'title' => 'JeCat PHP Framework' ,
 		) ;
 		$this->gitStatus($arrAllReposStatus['framework']) ;
 			
 		$arrAllReposStatus['platform'] = array(
 				'type' => '平台' ,
-				'folder' => FileSystem::singleton()->findFolder('/') ,
+				'folder' => Folder::singleton()->findFolder('/') ,
 				'title' => '蜂巢' ,
 		) ;
 		$this->gitStatus($arrAllReposStatus['platform']) ;
@@ -53,7 +53,7 @@ class ReposStatus extends ControlPanel
 			
 			$sExtName = $aExtMeta->name() ;
 			$arrAllReposStatus[$sExtName]['type'] = '扩展' ;
-			$arrAllReposStatus[$sExtName]['folder'] = FileSystem::singleton()->findFolder($aExtMeta->installPath()) ;
+			$arrAllReposStatus[$sExtName]['folder'] = Folder::singleton()->findFolder($aExtMeta->installPath()) ;
 			$arrAllReposStatus[$sExtName]['title'] = $aExtMeta->title() ;
 			
 			$this->gitStatus($arrAllReposStatus[$sExtName]) ;
@@ -69,7 +69,7 @@ class ReposStatus extends ControlPanel
 			
 			if($arrStatus['folder'] instanceof LocalFolder)
 			{
-				chdir($arrStatus['folder']->url(false)) ;
+				chdir($arrStatus['folder']->path()) ;
 				$arrStatus['detail'] = `git status -s` ;
 				$arrStatus['status'] = empty($arrStatus['detail'])? 'clean': 'modified' ;
 				$arrStatus['unpush'] = preg_match("|# Your branch is ahead of '([\\w-_]+?)/([\\w-_]+?)' by (\\d+) commit.|", `git status`,$arrRes)?
