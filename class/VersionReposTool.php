795975d2464e4b2d97bbf7437dcb46a0102cc937
alee <zq@alee2002.com>
2012-04-09 14:27:41 +0800

    使用menu buildBean 事件 替代 AOP

--- class/VersionReposTool.php
+++ class/VersionReposTool.php
@@ -3,11 +3,28 @@ namespace org\opencomb\development\repos ;
 
 use org\jecat\framework\lang\aop\AOP;
 use org\opencomb\platform\ext\Extension ;
+use org\opencomb\platform\mvc\view\widget\Menu;
+use org\jecat\framework\bean\BeanFactory;
 
 class VersionReposTool extends Extension 
 {
 	public function load()
 	{
-		AOP::singleton()->register('org\\opencomb\\development\\repos\\aspect\\ControlPanelFrameAspect') ;
+		// 注册菜单build事件的处理函数
+		Menu::registerBuildHandle(
+				'org\\opencomb\\coresystem\\mvc\\controller\\ControlPanelFrame'
+				, 'frameView'
+				, 'mainMenu'
+				, array(__CLASS__,'buildControlPanelMenu')
+		) ;
+	}
+	
+	static public function buildControlPanelMenu(array & $arrConfig)
+	{
+		// 合并配置数组，增加菜单
+		BeanFactory::mergeConfig(
+				$arrConfig
+				, BeanFactory::singleton()->findConfig('widget/control-panel-frame-menu','mvc-merger')
+		) ;
 	}
 }
